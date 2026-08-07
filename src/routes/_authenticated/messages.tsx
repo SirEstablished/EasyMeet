f0f86f87ed91d5b8f5e866eed0a00c648ae417d4
SirEstablished <georgeanthonylegacy@gmail.com>
2026-08-07 03:35:40 +0100

    Adjust height of main container in messages component

--- src/routes/_authenticated/messages.tsx
+++ src/routes/_authenticated/messages.tsx
@@ -187,7 +187,7 @@ function MessagesPage() {
   if (!user) return null;
 
   return (
-    <div className="max-w-6xl mx-auto h-[calc(100dvh-4rem)] flex bg-background overflow-hidden">
+    <div className="max-w-6xl mx-auto h-[calc(100dvh-8rem)] flex bg-background overflow-hidden">
       {/* Sidebar */}
       <aside
         className={cn(
